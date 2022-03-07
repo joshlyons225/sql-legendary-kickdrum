f852006213ce1989c0c8457cb004624fd2b5f84d
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-03-07 10:21:21 -0600

    added all required router expressions to all js files in routes

--- db/schema.sql
+++ db/schema.sql
@@ -20,4 +20,4 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
     manager_id INTEGER
-)
+);
--- server.js
+++ server.js
@@ -2,21 +2,24 @@ const express = require('express');
 const db = require('./db/connection');
 const apiRoutes = require('./routes');
 
-const port = process.env.PORT || 3007;
+const PORT = process.env.PORT || 3007;
 const app = express();
 
 // setup middleware
 app.use(express.urlencoded({ entended: false }));
 app.use(express.json());
 
-// setup api routing
+// setup api routing and 404 response
 app.use('/api', apiRoutes);
+app.use((req, res) => {
+    res.status(404).end();
+});
 
 // setup server connection following database connect
 db.connect(err => {
     if (err) throw err;
     console.log('Database connected');
     app.listen(PORT, () => {
-        console.log(`Server rocking on ${PORT}`);
+        console.log(`Server rocking on port ${PORT} like a baller`);
     });
 });
