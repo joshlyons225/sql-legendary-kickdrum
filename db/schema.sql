e961a5677f997324f31b9311033c450545081153
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-03-07 14:04:26 -0600

    updated schema.sql with fks in role and employee tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,8 @@
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
+
+
 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -11,13 +13,15 @@ CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL
+    department_id INTEGER,
+    CONSTRAINT fk_dept FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL,
-    manager_id INTEGER
+    role_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
 );
