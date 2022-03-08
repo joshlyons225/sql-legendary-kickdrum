61778c5ed81be9ba45ae05bf477b0f5fa6dd4de2
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-03-07 22:09:55 -0600

    updated initializeApp() with functions for each case

--- server.js
+++ server.js
@@ -40,11 +40,37 @@ db.connect(err => {
 
 // initialize app inquirer prompts
 function initializeApp() {
-    inquirer
-        .prompt({
-            type: 'list',
-            name: 'task',
-            message: 'What you wanna do, boss?',
-            choices: ['View Employees', 'View Employees by Department', 'Add an Employee', 'Remove an Employee', 'Update Role', 'Add Role', 'Terminate Session']
-        })
+    inquirer.prompt({
+        type: 'list',
+        name: 'task',
+        message: 'What you wanna do, boss?',
+        choices: ['View Employees', 'View Employees by Department', 'Add an Employee', 'Remove an Employee', 'Update Role', 'Add Role', 'Terminate Session']
+    })
+    
+    // switch between task options above
+    .then(function ({ task }) {
+        switch (task) {
+            case 'View Employees':
+                viewEmployee();
+                break;
+            case 'View Employees by Department':
+                viewByDepartment();
+                break;
+            case 'Add an Employee':
+                addEmployee();
+                break;
+            case 'Remove an Employee':
+                removeEmployee();
+                break;
+            case 'Update Role':
+                updateRole();
+                break;
+            case 'Add Role':
+                    addRole();
+                break;
+            case 'Terminate Session':
+                db.end();
+                break;
+        }
+    });
 };
