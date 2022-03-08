0b21b7fdb69b30adf9b2c0008e0ed7b566ca4d2b
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-03-08 15:22:37 -0600

    added viewEmployee function and query code to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const res = require('express/lib/response');
 const inquirer = require('inquirer');
 const db = require('../db/connection');
 require('console.table')
@@ -40,8 +41,24 @@ function initializeApp() {
 };
 
 // viewEmployee function
-
-
+function viewEmployee() {
+    const query = `
+    SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role ON (role.id = employee.role_id)
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY employee.id;
+    `;
+    // query db for above function
+    db.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        console.log('You found an employee!\n')
+        // recall initializeApp function
+        initializeApp();
+    });
+};
 
 // viewByDepartment function
 
