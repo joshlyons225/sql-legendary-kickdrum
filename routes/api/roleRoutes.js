fab09d1607f9628588115530b8d91ae582cb54b8
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-03-08 10:20:22 -0600

    updated file tree to remove api folder and contents from routes directory and moved initalizeApp() to index.js

--- routes/api/roleRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const inquirer = require('inquirer');
-require('console.table') 
-
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const db = require('./db/connection');
-const inquirer = require('inquirer');
-const routes = require('./routes');
+const initializeApp = require('./routes/index');
 
 const PORT = process.env.PORT || 3007;
 const app = express();
@@ -9,7 +8,6 @@ const app = express();
 // setup middleware
 app.use(express.urlencoded({ entended: false }));
 app.use(express.json());
-app.use(routes);
 app.use((req, res) => {
     res.status(404).end();
 });
@@ -34,40 +32,3 @@ db.connect(err => {
     // call function to start app
     initializeApp();
 });
-
-// initialize app inquirer prompts
-function initializeApp() {
-    inquirer.prompt({
-        type: 'list',
-        name: 'task',
-        message: 'What you wanna do, boss?',
-        choices: ['View Employees', 'View Employees by Department', 'Add an Employee', 'Remove an Employee', 'Update Role', 'Add Role', 'Terminate Session']
-    })
-    
-    // switch between task options above
-    .then(function ({ task }) {
-        switch (task) {
-            case 'View Employees':
-                viewEmployee();
-                break;
-            case 'View Employees by Department':
-                viewByDepartment();
-                break;
-            case 'Add an Employee':
-                addEmployee();
-                break;
-            case 'Remove an Employee':
-                removeEmployee();
-                break;
-            case 'Update Role':
-                updateRole();
-                break;
-            case 'Add Role':
-                addRole();
-                break;
-            case 'Terminate Session':
-                db.end();
-                break;
-        }
-    });
-};
