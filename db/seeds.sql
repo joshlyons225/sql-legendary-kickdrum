e33e0a6f0581da3d2a99ba1d8012f41d3787a4b2
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-03-10 21:56:19 -0600

    updated schema and seeds sql files

---
+++ db/seeds.sql
@@ -0,0 +1,52 @@
+USE staff;
+
+INSERT INTO department (name)
+VALUES ('Marketing');
+INSERT INTO department (name)
+VALUES ('Administration');
+INSERT INTO department (name)
+VALUES ('Human Resources');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('BD Consultant', 65000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ('BD Manager', 85000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Admin Assistant', 40000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Ops Manager', 95000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Finance Manager', 95000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ('VP Operations', 115000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ('VP Finance', 115000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ('HR Assistant', 45000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ('HR Manager', 65000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ('HR Director', 85000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Josh', 'Lyons', 4, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Keith', 'Richards', 1, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Buddy', 'Guy', 2, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Jimmy', 'Page', 3, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Gary', 'Clark Jr', 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('David', 'Gilmour', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Stevie', 'Ray Vaughan', 3, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Billy', 'Gibbons', 1, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Slash', 'Rad', 2, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Jack', 'White', 1, 2);
+
+
