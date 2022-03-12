8151f8fa378e328611555f4d4b85f2653dde3268
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-03-12 11:38:12 -0600

    changed on delete to cascade

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER,
-    CONSTRAINT fk_dept FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    CONSTRAINT fk_dept FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
@@ -21,5 +21,5 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER,
-    CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
+    CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
 );
